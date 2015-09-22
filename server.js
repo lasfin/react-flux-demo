439c6dcc3719f8b931963f7306f0ff05fd6fc68a
Evgenij Kopitsa <e.kopitca@semrush.com>
2015-09-22 12:13:36 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+express()
+    .set('view engine', 'ejs')
+    .use(express.static('./public'))
+    .get('*', function(req, res){
+       res.render('index');
+    });
---
+++ src/main.js
@@ -0,0 +1,4 @@
+var events = require('events');
+
+console.log('g2');
+console.log(events);
