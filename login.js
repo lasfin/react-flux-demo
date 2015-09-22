cea425ed58f6503530c5f8c5e623a7cb8bbf68d5
Evgenij Kopitsa <e.kopitca@semrush.com>
2015-09-22 16:25:09 +0300

    login template

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ var LocallyDB = require('locallydb');
 var db = new LocallyDB('./data');
 var users = db.collection('users');
 
-var crypto = require('cryto');
+var crypto = require('crypto');
 
 function hash (password){
     return crypto.createhash('sha512').update(password).digest('hex');
@@ -34,3 +34,23 @@ passport.serializeUser(function(user, done){
 passport.deserializeUser(function(cid, done){
     done(null, users.get(cid));
 });
+
+var router = require('express').Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true})); // Login Page
+router.use(bodyParser.json()); //API
+router.use(require('cookie-parser')());
+router.use(require('express-session')({
+    secret: 'fdsjoifjdsiofjdsg43tdgdsgdfgdf',
+    resave: false,
+    saveUninitialized: true
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/login', function(req, res){
+    res.render('login')
+});
+
+exports.routes = router;
